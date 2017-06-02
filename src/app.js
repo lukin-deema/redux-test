a6e332627f483bf70ec636eda0570622cfcec29f
lukin-deema <lukin.d8709@gmail.com>
2017-06-02 22:34:56 +0300

    redux. tutorial sample

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import MainPage from './components/Main';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import reducer from './reducers';
 
-ReactDOM.render(
-    <MainPage/>,
+const store = createStore(reducer);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
